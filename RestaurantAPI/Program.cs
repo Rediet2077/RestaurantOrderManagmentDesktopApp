100cc13de673a12b7ccb1f63d175287c9cd62090
kalk-24 <fisehakalkidan72@gmail.com>
2026-04-18 23:35:23 +0300

    Implement API logic part 7

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -40,3 +40,4 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
 // Additional feature code part 6
+// Additional feature code part 7
